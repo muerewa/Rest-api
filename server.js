384b12663486f164c3f96f90e76eec5545a520d6
muerewaa <muerewaa@gmail.com>
2021-10-12 20:02:28 +0300

    fixed bugs

--- server.js
+++ server.js
@@ -3,17 +3,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
 const router = require('./router')
-const port = 3000;
+const port = 3000
 
 let url = config.get("url") // Создаем переменную с url для подключения к mongodb
 const app = express()
 
 app.use(express.json()) // Заставляем express парсить json
 app.use('/api', router) // Подключаем роутер
-
-app.get('/', (res,req) => {
-    res.sendFile('index.html')
-})
 
 
 async function startServer() {
